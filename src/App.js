94932f8f1441d58490c16c44988e225151069bba
linhkhanh98 <linhkhnguyen.98@gmail.com>
2025-07-03 21:34:38 -0700

    completed task function

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [allTodos, setTodos] = useState([]); // an array of todo items
   const [newTitle, setNewTitle] = useState(""); // new string
   const [newDescription, setNewDescription] = useState("");
+  const [completedTodos, setCompletedTodos] = useState([]);
 
   // updating new titile and description function
   const handleAddTodo = () => {
@@ -32,6 +33,33 @@ function App() {
     // localStorage.removeItem(reducedTodo); // this wont delete localStorage
     setTodos(reducedTodo);
   };
+
+  const handleComplete = index => {
+    let now = new Date ();
+    let dd = now.getDate ();
+    let mm = now.getMonth () + 1;
+    let yyyy = now.getFullYear ();
+    let h = now.getHours ();
+    let m = now.getMinutes ();
+    let s = now.getSeconds ();
+    let completedOn =
+      dd + '-' + mm + '-' + yyyy + ' at ' + h + ':' + m + ':' + s;
+
+    let filteredItem = {
+      ...allTodos[index],
+      completedOn: completedOn,
+    };
+
+    let updatedCompletedArr = [...completedTodos];
+    updatedCompletedArr.push (filteredItem);
+    setCompletedTodos (updatedCompletedArr);
+    handleDeleteTodo (index);
+    localStorage.setItem (
+      'completedTodos',
+      JSON.stringify (updatedCompletedArr)
+    );
+  };
+
   // use effect when the page is rendered first time, we will be checking to see if 
   // there's any items in local storage or not
   useEffect (() => {
@@ -86,7 +114,7 @@ function App() {
           </div>
           {/* list of todo list */}
           <div className="todo-list">
-            {allTodos.map((item, index)=>{
+            {isCompleteScreen === false && allTodos.map((item, index)=>{
               return(
                 <div className="todo-list-item" key={index}>
                   <div>
@@ -99,7 +127,29 @@ function App() {
                     onClick={() => handleDeleteTodo(index)}
                     title="Delete?"
                     />
-                    <IoCheckmarkDoneSharp className='check-icon'/>
+                    <IoCheckmarkDoneSharp 
+                    className='check-icon' 
+                    onClick={() => handleComplete(index)}
+                    title="Complete?"
+                    />
+                  </div>
+                </div>
+              )
+            })}
+            {isCompleteScreen === true && completedTodos.map((item, index)=>{
+              return(
+                <div className="todo-list-item" key={index}>
+                  <div>
+                    <h3>{item.title}</h3>
+                      <p>{item.description}</p>
+                      <p><i>Completed on: {item.completedOn}</i></p>
+                  </div>
+                  <div>
+                    <RiDeleteBin5Fill 
+                    className='icon' 
+                    onClick={() => handleDeleteTodo(index)}
+                    title="Delete?"
+                    />
                   </div>
                 </div>
               )
